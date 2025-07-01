0338569b44e255486eb88beb97484bd40700216b
Elias Bariani <Elias>
2025-07-01 20:21:00 -0300

    feat: add Swagger integration and document RegisterUserInput DTO

--- src/modules/users/application/use-cases/dtos/register-user.input.ts
+++ src/modules/users/application/use-cases/dtos/register-user.input.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDateString,
   IsNotEmpty,
@@ -8,17 +9,21 @@ import {
 } from 'class-validator';
 
 export class RegisterUserInput {
+  @ApiProperty({ example: 'João da Silva' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ example: '12345678900' })
   @IsNotEmpty()
   @Matches(/^[0-9]{11}$/, { message: 'CPF must be 11 digits' })
   cpf: string;
 
+  @ApiProperty({ example: '2000-01-01' })
   @IsDateString()
   birthDate: Date;
 
+  @ApiProperty({ example: 2500.75 })
   @IsNumber()
   @Min(0)
   income: number;
