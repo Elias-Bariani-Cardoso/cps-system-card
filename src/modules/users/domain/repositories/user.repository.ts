64e24261655b4332c8ce7b500237b5d6877fd47a
Elias Bariani <Elias>
2025-07-02 16:43:42 -0300

    feat: add user services and repository methods

--- src/modules/users/domain/repositories/user.repository.ts
+++ src/modules/users/domain/repositories/user.repository.ts
@@ -1,8 +1,11 @@
-// user.repository.ts (interface)
 import { User } from '../entities/user.entity';
+import { Cpf } from '../value-objects/cpf.vo';
 
 export const USER_REPOSITORY = Symbol('USER_REPOSITORY');
 
 export interface UserRepository {
   save(user: User): Promise<User>;
+  findAll(): Promise<User[]>;
+  findByCpf(cpf: Cpf): Promise<User | null>;
+  deleteByCpf(cpf: Cpf): Promise<void>;
 }
